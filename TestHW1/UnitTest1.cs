2e63b8392d1bda48d321618095a303aa1735aba7
Briana Davis <brianad570@gmail.com>
2025-08-28 18:02:35 -0400

    Resolved Enhancement Issue

--- TestHW1/UnitTest1.cs
+++ TestHW1/UnitTest1.cs
@@ -1,26 +1,30 @@
-﻿using ClassLibraryHW1;
-
+﻿using System.Diagnostics.CodeAnalysis;
+using ClassLibraryHW1;
 namespace TestHW1;
 
 public class UnitTest1
 {
     private readonly Class1 passwordChecker = new();
 
+    // Password Length Check
+    [Fact] public void Returns_Error_Message_For_Short_Password() => Assert.Equal("INCORRECT PASSWORD LENGTH. Passwords must have 8 characters.", passwordChecker.CheckPassword("1vds"));
+
+    // Ineligible Cases (0 Tests Passed)
     [Fact] public void Returns_Ineligible_WhenEmpty() => Assert.Equal("INELIGIBLE", passwordChecker.CheckPassword(""));
 
     // Weak Cases (1 Tests Passed)
-    [Fact] public void Returns_Weak_For_One_Criteria_Lowercase() => Assert.Equal("WEAK", passwordChecker.CheckPassword("awsty"));
-    [Fact] public void Returns_Weak_For_One_Criteria_Uppercase() => Assert.Equal("WEAK", passwordChecker.CheckPassword("QWERTY"));
-    [Fact] public void Returns_Weak_For_One_Criteria_Number() => Assert.Equal("WEAK", passwordChecker.CheckPassword("6473"));
-    [Fact] public void Returns_Weak_For_One_Criteria() => Assert.Equal("WEAK", passwordChecker.CheckPassword("awsty"));
+    [Fact] public void Returns_Weak_For_One_Criteria_Lowercase() => Assert.Equal("WEAK", passwordChecker.CheckPassword("awstyrffgbtrtb"));
+    [Fact] public void Returns_Weak_For_One_Criteria_Uppercase() => Assert.Equal("WEAK", passwordChecker.CheckPassword("QWERTYJSNKNSCJKA"));
+    [Fact] public void Returns_Weak_For_One_Criteria_Number() => Assert.Equal("WEAK", passwordChecker.CheckPassword("64731727382"));
+    [Fact] public void Returns_Weak_For_One_Criteria() => Assert.Equal("WEAK", passwordChecker.CheckPassword("!!!!@@@@!!!!!"));
 
     // Medium Cases (2 Tests Passed)
-    [Fact] public void Returns_Medium_For_Two_Criteria_LowerUpperCase() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("awstyEERT"));
-    [Fact] public void Returns_Medium_For_Two_Criteria_LowercaseNumber() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("djc1524"));
-    [Fact] public void Returns_Medium_For_Two_Criteria_LowercaseSymbol() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("adj$@#"));
-    [Fact] public void Returns_Medium_For_Two_Criteria_UppercaseNumber() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("1234EERT"));
-    [Fact] public void Returns_Medium_For_Two_Criteria_UpperSymbol() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("#$FTRE"));
-    [Fact] public void Returns_Medium_For_Two_Criteria_SymbolNumber() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("1234$%%^"));
+    [Fact] public void Returns_Medium_For_Two_Criteria_LowerUpperCase() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("awstyEERTTTVX"));
+    [Fact] public void Returns_Medium_For_Two_Criteria_LowercaseNumber() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("djc1524xcmsc"));
+    [Fact] public void Returns_Medium_For_Two_Criteria_LowercaseSymbol() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("adj$@#scasc"));
+    [Fact] public void Returns_Medium_For_Two_Criteria_UppercaseNumber() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("1234EERT1222"));
+    [Fact] public void Returns_Medium_For_Two_Criteria_UpperSymbol() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("#$FTREFGGTG"));
+    [Fact] public void Returns_Medium_For_Two_Criteria_SymbolNumber() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("1234$%%^23433"));
 
     // Medium Cases (3 Tests Passed)
     [Fact] public void Returns_Medium_For_Three_Criteria_UpperLowerCaseNumber() => Assert.Equal("MEDIUM", passwordChecker.CheckPassword("1234EERTcjcna"));
--- TestHW1/obj/Debug/net9.0/TestHW1.AssemblyInfo.cs
+++ TestHW1/obj/Debug/net9.0/TestHW1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestHW1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94b664b07242b9bacd1404c5f05ed9735733b5a8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95a2dac0df5297708628f6428335f0efc272dc8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestHW1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestHW1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
